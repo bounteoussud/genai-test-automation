97d62a916f8d12f71f6165f556634a12dc2c4226
Vikas Ahuja <vikas.ahuja@insightsoftware.com>
2025-12-24 09:09:29 +0530

    adding modified file for push

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 #from dotenv import load_dotenv
 #load_dotenv()  # MUST be first
+import os
+
+os.environ["LITELLM_LOG"] = "ERROR"
+os.environ["LITELLM_DISABLE_TELEMETRY"] = "true"
+os.environ["LITELLM_MODE"] = "NONE"   # 👈 important
 
 from core.crew import run_crew
 
